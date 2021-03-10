f1162368f794d767059cc93b01421ff27f605da8
Hamza Aslam <70750995+hmz-23@users.noreply.github.com>
2021-03-10 17:35:29 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,s;
+        System.out.println("Enter two numbers");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        s= a+b;
+        System.out.println("Sum="+s);
+
+    }
+}
